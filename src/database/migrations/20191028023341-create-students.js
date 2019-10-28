e0538de464ce615e14d5a534975fd82dc7348f2e
BenhurKulzer <benhur.bitpc@gmail.com>
2019-10-27 23:35:10 -0300

    Migration - Students

--- src/database/migrations/20191028023341-create-students.js
+++ src/database/migrations/20191028023341-create-students.js
@@ -16,14 +16,17 @@ module.exports = {
                 allowNull: false,
                 unique: true,
             },
-            password_hash: {
+            age: {
                 type: Sequelize.STRING,
                 allowNull: false,
             },
-            provider: {
-                type: Sequelize.BOOLEAN,
+            weight: {
+                type: Sequelize.STRING,
                 allowNull: false,
-                defaultValue: false,
+            },
+            height: {
+                type: Sequelize.STRING,
+                allowNull: false,
             },
             created_at: {
                 type: Sequelize.DATE,
